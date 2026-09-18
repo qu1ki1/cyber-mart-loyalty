6ca060871bad8df0e11275db978cfcf548c22956
qu1ki1 <the.quiki.life@gmail.com>
2026-09-18 18:03:23 +0500

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,8 +1,57 @@
-export default function handler(req, res) {
-  console.log("Telegram API works");
+import TelegramBot from "node-telegram-bot-api";
+import { createClient } from "@supabase/supabase-js";
+
+const bot = new TelegramBot(process.env.BOT_TOKEN);
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
+
+export default async function handler(req, res) {
+
+  const message = req.body?.message;
+
+  if (!message) {
+    return res.status(200).json({ ok: true });
+  }
+
+  const chatId = message.chat.id;
+
+  if (message.text === "/start") {
+
+    await supabase
+      .from("users")
+      .insert({
+        telegram_id: chatId,
+        username: message.from.username,
+        first_name: message.from.first_name
+      });
+
+
+    await bot.sendMessage(
+      chatId,
+      "🎮 CYBER MART\n\nТвой подарок за визит 👇",
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "🎁 Получить подарок",
+                web_app: {
+                  url: "https://cyber-mart-loyalty.vercel.app"
+                }
+              }
+            ]
+          ]
+        }
+      }
+    );
+  }
+
 
   res.status(200).json({
-    ok: true,
-    message: "Telegram webhook ready"
+    ok:true
   });
+
 }
