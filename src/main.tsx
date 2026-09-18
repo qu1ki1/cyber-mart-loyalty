60361d6840ca8b619b41fcfcbcaf2c646bc2c8f4
qu1ki1 <the.quiki.life@gmail.com>
2026-09-19 00:15:53 +0500

    add admin routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,75 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 
-// @ts-ignore
+import App from './App.tsx'
 import Admin from './Admin.jsx'
+
+import Dashboard from './admin/Dashboard.jsx'
+import Users from './admin/Users.jsx'
+import Gifts from './admin/Gifts.jsx'
+import Winners from './admin/Winners.jsx'
+import Settings from './admin/Settings.jsx'
 
 import WebApp from '@twa-dev/sdk'
 
-if (WebApp && typeof WebApp.ready === "function") {
+
+if(WebApp && typeof WebApp.ready==="function"){
   WebApp.ready()
 }
 
-if (WebApp && typeof WebApp.expand === "function") {
+
+if(WebApp && typeof WebApp.expand==="function"){
   WebApp.expand()
 }
 
+
+
 const path = window.location.pathname
 
+
+
+function Router(){
+
+
+if(path === "/admin"){
+  return <Admin/>
+}
+
+
+if(path === "/admin/users"){
+  return <Users/>
+}
+
+
+if(path === "/admin/gifts"){
+  return <Gifts/>
+}
+
+
+if(path === "/admin/winners"){
+  return <Winners/>
+}
+
+
+if(path === "/admin/settings"){
+  return <Settings/>
+}
+
+
+return <App/>
+
+
+}
+
+
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    {
-      path === "/admin"
-        ? <Admin />
-        : <App />
-    }
-  </StrictMode>
+
+<StrictMode>
+
+<Router/>
+
+</StrictMode>
+
 )
