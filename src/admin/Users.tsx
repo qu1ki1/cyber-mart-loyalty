d352b8474ccb3db3769c23f3b138e20d4ed1f88d
qu1ki1 <the.quiki.life@gmail.com>
2026-09-19 01:16:01 +0500

    fix typescript type import

--- src/admin/Users.tsx
+++ src/admin/Users.tsx
@@ -1,84 +1,81 @@
 import { useEffect, useState } from "react";
-import { getUsers, User } from "./supabaseAdmin";
+import { getUsers } from "./supabaseAdmin";
+import type { User } from "./supabaseAdmin";
 
 
 export default function Users(){
 
-const [users,setUsers] = useState<User[]>([]);
+  const [users,setUsers] = useState<User[]>([]);
 
 
-useEffect(()=>{
+  useEffect(()=>{
 
-getUsers()
-.then(setUsers)
+    getUsers().then(setUsers);
 
-},[]);
-
+  },[]);
 
 
-return (
 
-<div>
+  return (
 
-<h1>
-👥 Пользователи
-</h1>
+    <div>
 
-
-<table>
-
-<thead>
-<tr>
-<th>ID</th>
-<th>Имя</th>
-<th>Username</th>
-<th>Попытки</th>
-<th>Подарок</th>
-</tr>
-</thead>
+      <h1>
+        👥 Пользователи
+      </h1>
 
 
-<tbody>
+      <table>
 
-{
-users.map((user)=>(
-
-<tr key={user.id}>
-
-<td>
-{user.telegram_id}
-</td>
-
-<td>
-{user.first_name || "-"}
-</td>
-
-<td>
-{user.username ? "@"+user.username : "-"}
-</td>
-
-<td>
-{user.attempts}
-</td>
-
-<td>
-{user.gift || "-"}
-</td>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Имя</th>
+            <th>Username</th>
+            <th>Попытки</th>
+            <th>Подарок</th>
+          </tr>
+        </thead>
 
 
-</tr>
+        <tbody>
 
-))
-}
+        {
+          users.map((user)=>(
+            
+            <tr key={user.id}>
 
-</tbody>
+              <td>
+                {user.telegram_id}
+              </td>
 
+              <td>
+                {user.first_name || "-"}
+              </td>
 
-</table>
+              <td>
+                {user.username ? "@" + user.username : "-"}
+              </td>
 
+              <td>
+                {user.attempts}
+              </td>
 
-</div>
+              <td>
+                {user.gift || "-"}
+              </td>
 
-)
+            </tr>
+
+          ))
+        }
+
+        </tbody>
+
+      </table>
+
+    </div>
+
+  );
 
 }
