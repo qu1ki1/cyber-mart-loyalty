3b0e78964de3ef47d57840842473a3de516060cd
qu1ki1 <the.quiki.life@gmail.com>
2026-09-19 00:04:33 +0500

    upgrade gifts admin panel

--- src/admin/Gifts.jsx
+++ src/admin/Gifts.jsx
@@ -8,47 +8,97 @@ const [gifts,setGifts]=useState([])
 
 const [name,setName]=useState("")
 const [chance,setChance]=useState("")
+const [quantity,setQuantity]=useState("")
 
 
-async function load(){
+async function loadGifts(){
 
-const {data}=await supabase
+const {data,error}=await supabase
 .from("gifts")
 .select("*")
 .order("id")
 
-setGifts(data || [])
+
+if(!error){
+setGifts(data)
+}
 
 }
 
 
+
 useEffect(()=>{
 
-load()
+loadGifts()
 
 },[])
 
 
 
+
 async function addGift(){
 
-if(!name)return
+if(!name) return
 
 
 await supabase
 .from("gifts")
 .insert({
-name,
-chance:Number(chance)
+
+name:name,
+
+chance:Number(chance),
+
+quantity:Number(quantity)
+
 })
 
 
 setName("")
 setChance("")
+setQuantity("")
 
-load()
+
+loadGifts()
 
 }
+
+
+
+
+
+async function deleteGift(id){
+
+await supabase
+.from("gifts")
+.delete()
+.eq("id",id)
+
+
+loadGifts()
+
+}
+
+
+
+
+async function toggleGift(id,status){
+
+await supabase
+.from("gifts")
+.update({
+
+active:!status
+
+})
+.eq("id",id)
+
+
+loadGifts()
+
+}
+
+
 
 
 
@@ -62,34 +112,64 @@ return (
 </h1>
 
 
+
 <div style={{
 padding:20,
 border:"1px solid #333",
-borderRadius:15
+borderRadius:15,
+marginBottom:30
 }}>
 
 
-<h3>
-Добавить подарок
-</h3>
+<h2>
+➕ Добавить подарок
+</h2>
 
 
 <input
+
 placeholder="Название"
+
 value={name}
-onChange={e=>setName(e.target.value)}
+
+onChange={
+e=>setName(e.target.value)
+}
+
 />
 
 
 <input
+
 placeholder="Шанс %"
+
 value={chance}
-onChange={e=>setChance(e.target.value)}
+
+onChange={
+e=>setChance(e.target.value)
+}
+
 />
+
+
+<input
+
+placeholder="Количество"
+
+value={quantity}
+
+onChange={
+e=>setQuantity(e.target.value)
+}
+
+/>
+
 
 
 <button onClick={addGift}>
+
 Добавить
+
 </button>
 
 
@@ -97,52 +177,116 @@ onChange={e=>setChance(e.target.value)}
 
 
 
+
+
 <h2>
-Список подарков
+Все подарки
 </h2>
 
 
 
+<div style={{
+display:"grid",
+gap:15
+}}>
+
+
 {
+
 gifts.map(g=>(
 
+
 <div
+
 key={g.id}
+
 style={{
-padding:15,
-margin:10,
-border:"1px solid #555",
-borderRadius:10
+
+border:"1px solid #444",
+
+borderRadius:15,
+
+padding:20
+
 }}
+
 >
 
 
+<h2>
 🎁 {g.name}
-
-<br/>
-
-Шанс:
-{g.chance}%
+</h2>
 
 
-<br/>
+<p>
+🎯 Шанс: {g.chance}%
+</p>
+
+
+<p>
+📦 Осталось: {g.quantity}
+</p>
+
+
+<p>
 
 {
-g.active 
-?"✅ Активен"
-:"❌ Выключен"
+g.active
+?
+"🟢 Активен"
+:
+"🔴 Выключен"
 }
+
+</p>
+
+
+
+<button
+
+onClick={()=>
+toggleGift(g.id,g.active)
+}
+
+>
+
+Изменить статус
+
+</button>
+
+
+
+<button
+
+onClick={()=>
+deleteGift(g.id)
+}
+
+>
+
+Удалить
+
+</button>
+
 
 
 </div>
+
 
 ))
+
+
 }
+
+
+</div>
 
 
 
 </div>
 
+
 )
+
 
 }
