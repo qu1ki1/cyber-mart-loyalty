4817d7d001964ba26328fcdfe42ceded54dd4ff4
qu1ki1 <the.quiki.life@gmail.com>
2026-09-18 19:01:59 +0500

    fix admin route

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+
+// @ts-ignore
 import Admin from './Admin.jsx'
+
 import WebApp from '@twa-dev/sdk'
 
 if (WebApp && typeof WebApp.ready === "function") {
@@ -13,7 +16,6 @@ if (WebApp && typeof WebApp.expand === "function") {
   WebApp.expand()
 }
 
-
 const path = window.location.pathname
 
 createRoot(document.getElementById('root')!).render(
@@ -23,5 +25,5 @@ createRoot(document.getElementById('root')!).render(
         ? <Admin />
         : <App />
     }
-  </StrictMode>,
+  </StrictMode>
 )
